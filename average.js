269b7bba0cc2df9e5c815e5dbce57ee8513e9c34
ayush91985 <67649316+ayush91985@users.noreply.github.com>
2021-12-19 00:47:05 +0530

    Update average.js
    nan if

--- average.js
+++ average.js
@@ -1,9 +1,8 @@
 
 function average(numbers) {
-  if(isNaN(numbers)
+  if(!(isNaN(numbers))
      
-     var a=0;
-     else
+    
      {
   return numbers.reduce((p, c)=> p + c, 0) / numbers.length;}
 }
